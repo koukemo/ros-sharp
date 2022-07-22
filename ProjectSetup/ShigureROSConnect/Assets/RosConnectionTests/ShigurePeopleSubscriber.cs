f70f488243711dccab83c6bd196f9727acd10358
koukemo <java.sparrow22.surface@gmail.com>
2022-07-23 01:24:49 +0900

    [feat] 関節点とそれらの接続部分を描画

--- ProjectSetup/ShigureROSConnect/Assets/RosConnectionTests/ShigurePeopleSubscriber.cs
+++ ProjectSetup/ShigureROSConnect/Assets/RosConnectionTests/ShigurePeopleSubscriber.cs
@@ -24,10 +24,10 @@ public class ShigurePeopleSubscriber : UnitySubscriber<RosSharp.RosBridgeClient.
     protected override void Start() 
     {
         _prefabBodyPart = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        _prefabBodyPart.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+        _prefabBodyPart.transform.localScale = new Vector3(50.0f, 50.0f, 50.0f);
         _prefabBodyPart.name = "_prefabBodyPart";
         _prefabEdge = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        _prefabEdge.transform.localScale = new Vector3(0.15f, 0.15f, 0.15f);
+        _prefabEdge.transform.localScale = new Vector3(25.0f, 25.0f, 25.0f);
         _prefabEdge.name = "_prefabEdge";
 
         base.Start();
@@ -81,9 +81,7 @@ public class ShigurePeopleSubscriber : UnitySubscriber<RosSharp.RosBridgeClient.
         GameObject gameObject = Instantiate(_prefabBodyPart, Vector3.zero, Quaternion.identity);
         gameObject.name = body_part_info_list.part_name;
         gameObject.transform.parent = parentObject.transform;
-        gameObject.GetComponent<Renderer>().material.color =    body_part_info_list.color;
-
-        create_flag = false;
+        gameObject.GetComponent<Renderer>().material.color = body_part_info_list.color;
     }
 
     private void CreateEdge(GameObject part1, GameObject part2)
@@ -107,9 +105,50 @@ public class ShigurePeopleSubscriber : UnitySubscriber<RosSharp.RosBridgeClient.
         gameObject.transform.parent = parentObject.transform;
     }
 
+    private void MoveBodyPart()
+    {
+        GameObject bodypart;
+        Vector3 now_pos = transform.position;
+        for (int i = 0; i < bodyPartInfos.Length; i++)
+        {
+            bodypart = GameObject.Find(bodyPartInfos[i].part_name);
+            now_pos.x = bodyPartInfos[i].x;
+            now_pos.y = bodyPartInfos[i].y;
+            now_pos.z = bodyPartInfos[i].z;
+
+            if (bodyPartInfos[i].x == 0.0f && bodyPartInfos[i].y == 0.0f && bodyPartInfos[i].z == 0.0f)
+            {
+                bodypart.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            } else
+            {
+                if (bodypart.transform.localScale == Vector3.zero)
+                {
+                    bodypart.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                }
+                bodypart.transform.position = now_pos;
+            }
+            
+        }
+    }
+
+    private void MoveEdge(GameObject edge, GameObject part1, GameObject part2)
+    {
+        // 位置の調整
+        float distance = Vector3.Distance(part1.transform.position, part2.transform.position);
+        edge.transform.localScale = new Vector3(edge.transform.localScale.x, edge.transform.localScale.y, distance);  // エッジの長さを変更
+
+        edge.transform.position = (part1.transform.position + part2.transform.position) * 0.5f;   // エッジの中心を変更
+
+        edge.transform.LookAt(part1.transform);   // エッジの角度を変更
+
+        unityPoseList.EdgeActiveControls();
+    }
+
+    
+
     private void CreateEdgeSet()
     {
-        // TODO : Edgeが正しく表示されない
+        unityPoseList.find_bodypart();
         // face 
         CreateEdge(unityPoseList.nose, unityPoseList.left_eye);
         CreateEdge(unityPoseList.nose, unityPoseList.right_eye);
@@ -143,6 +182,45 @@ public class ShigurePeopleSubscriber : UnitySubscriber<RosSharp.RosBridgeClient.
         CreateEdge(unityPoseList.right_big_toe, unityPoseList.right_small_toe);
     }
 
+    private void MoveEdgeSet()
+    {
+        unityPoseList.find_bodypart();
+        unityPoseList.find_bodypart_edge();
+        // face 
+        MoveEdge(unityPoseList.noseTOleft_eye, unityPoseList.nose, unityPoseList.left_eye);
+        MoveEdge(unityPoseList.noseTOright_eye, unityPoseList.nose, unityPoseList.right_eye);
+        MoveEdge(unityPoseList.left_eyeTOleft_ear, unityPoseList.left_eye, unityPoseList.left_ear);
+        MoveEdge(unityPoseList.right_eyeTOright_ear, unityPoseList.right_eye, unityPoseList.right_ear);
+
+        // body
+        MoveEdge(unityPoseList.noseTOneck, unityPoseList.nose, unityPoseList.neck);
+        MoveEdge(unityPoseList.neckTObackground, unityPoseList.neck, unityPoseList.background);
+
+        // arm
+        MoveEdge(unityPoseList.neckTOleft_shoulder, unityPoseList.neck, unityPoseList.left_shoulder);
+        MoveEdge(unityPoseList.neckTOright_shoulder, unityPoseList.neck, unityPoseList.right_shoulder);
+        MoveEdge(unityPoseList.left_shoulderTOleft_elbow, unityPoseList.left_shoulder, unityPoseList.left_elbow);
+        MoveEdge(unityPoseList.right_shoulderTOright_elbow, unityPoseList.right_shoulder, unityPoseList.right_elbow);
+        MoveEdge(unityPoseList.left_elbowTOleft_wrist, unityPoseList.left_elbow, unityPoseList.left_wrist);
+        MoveEdge(unityPoseList.right_elbowTOright_wrist, unityPoseList.right_elbow, unityPoseList.right_wrist);
+
+        // leg
+        MoveEdge(unityPoseList.backgroundTOleft_hip, unityPoseList.background, unityPoseList.left_hip);
+        MoveEdge(unityPoseList.backgroundTOright_hip, unityPoseList.background, unityPoseList.right_hip);
+        MoveEdge(unityPoseList.left_hipTOleft_knee, unityPoseList.left_hip, unityPoseList.left_knee);
+        MoveEdge(unityPoseList.right_hipTOright_knee, unityPoseList.right_hip, unityPoseList.right_knee);
+        MoveEdge(unityPoseList.left_kneeTOleft_ankle, unityPoseList.left_knee, unityPoseList.left_ankle);
+        MoveEdge(unityPoseList.right_kneeTOright_ankle, unityPoseList.right_knee, unityPoseList.right_ankle);
+        MoveEdge(unityPoseList.left_ankleTOleft_big_toe, unityPoseList.left_ankle, unityPoseList.left_big_toe);
+        MoveEdge(unityPoseList.left_ankleTOleft_heel, unityPoseList.left_ankle, unityPoseList.left_heel);
+        MoveEdge(unityPoseList.right_ankleTOright_big_toe, unityPoseList.right_ankle, unityPoseList.right_big_toe);
+        MoveEdge(unityPoseList.right_ankleTOright_heel, unityPoseList.right_ankle, unityPoseList.right_heel);
+        MoveEdge(unityPoseList.left_big_toeTOleft_small_toe, unityPoseList.left_big_toe, unityPoseList.left_small_toe);
+        MoveEdge(unityPoseList.right_big_toeTOright_small_toe, unityPoseList.right_big_toe, unityPoseList.right_small_toe);
+
+        unityPoseList.EdgeActiveControls();
+    }
+
     public void Update()
     {
         if(receivedMsg)
@@ -150,13 +228,21 @@ public class ShigurePeopleSubscriber : UnitySubscriber<RosSharp.RosBridgeClient.
             // debug logにpeople_id[0]と, その人の関節点情報を出力
             DebugMessage();
 
+            // Cube Humanoidの骨格作成
             if (create_flag){
                 for (int i = 0; i < bodyPartInfos.Length; i++)
                 {
                     CreateBodyPart(bodyPartInfos[i]);
                 }
                 CreateEdgeSet();
+
+                GameObject.Find("_prefabBodyPart").SetActive(false);
+                GameObject.Find("_prefabEdge").SetActive(false);
+                create_flag = false;
             }
+
+            MoveBodyPart();
+            MoveEdgeSet();
 
             receivedMsg = false;
         }
--- ProjectSetup/ShigureROSConnect/Assets/RosConnectionTests/UnityPoseList.cs
+++ ProjectSetup/ShigureROSConnect/Assets/RosConnectionTests/UnityPoseList.cs
@@ -3,37 +3,182 @@ using UnityEngine;
 [System.Serializable]
 public class UnityPoseList
 {
-    public GameObject nose = GameObject.Find( "nose" );
-    public GameObject neck = GameObject.Find( "neck" );
+    // Body part
+    public GameObject nose;
+    public GameObject neck;
 
-    public GameObject right_shoulder = GameObject.Find( "right_shoulder" );
-    public GameObject right_elbow = GameObject.Find( "right_elbow" );
-    public GameObject right_wrist = GameObject.Find( "right_wrist" );
+    public GameObject right_shoulder;
+    public GameObject right_elbow;
+    public GameObject right_wrist;
 
-    public GameObject left_shoulder = GameObject.Find( "left_shoulder" );
-    public GameObject left_elbow = GameObject.Find( "left_elbow" );
-    public GameObject left_wrist = GameObject.Find( "left_wrist" );
+    public GameObject left_shoulder;
+    public GameObject left_elbow;
+    public GameObject left_wrist;
 
-    public GameObject right_hip = GameObject.Find( "right_hip" );
-    public GameObject right_knee = GameObject.Find( "right_knee" );
-    public GameObject right_ankle = GameObject.Find( "right_ankle" );
+    public GameObject right_hip;
+    public GameObject right_knee;
+    public GameObject right_ankle;
 
-    public GameObject left_hip = GameObject.Find( "left_hip" );
-    public GameObject left_knee = GameObject.Find( "left_knee" );
-    public GameObject left_ankle = GameObject.Find( "left_ankle" );
+    public GameObject left_hip;
+    public GameObject left_knee;
+    public GameObject left_ankle;
 
-    public GameObject right_eye = GameObject.Find( "right_eye" );
-    public GameObject left_eye = GameObject.Find( "left_eye" );
-    public GameObject right_ear = GameObject.Find( "right_ear" );
-    public GameObject left_ear = GameObject.Find( "left_ear" );
+    public GameObject right_eye;
+    public GameObject left_eye;
+    public GameObject right_ear;
+    public GameObject left_ear;
 
-    public GameObject left_big_toe = GameObject.Find( "left_big_toe" );
-    public GameObject left_small_toe = GameObject.Find( "left_small_toe" );
-    public GameObject left_heel = GameObject.Find( "left_heel" );
+    public GameObject left_big_toe;
+    public GameObject left_small_toe;
+    public GameObject left_heel;
 
-    public GameObject right_big_toe = GameObject.Find( "right_big_toe" );
-    public GameObject right_small_toe = GameObject.Find( "right_small_toe" );
-    public GameObject right_heel = GameObject.Find( "right_heel" );
+    public GameObject right_big_toe;
+    public GameObject right_small_toe;
+    public GameObject right_heel;
 
-    public GameObject background = GameObject.Find( "background" );
+    public GameObject background;
+
+    // Body part edge
+    public GameObject noseTOleft_eye;
+    public GameObject noseTOright_eye;
+    public GameObject left_eyeTOleft_ear;
+    public GameObject right_eyeTOright_ear;
+
+    public GameObject noseTOneck;
+    public GameObject neckTObackground;
+
+    public GameObject neckTOleft_shoulder;
+    public GameObject neckTOright_shoulder;
+    public GameObject left_shoulderTOleft_elbow;
+    public GameObject right_shoulderTOright_elbow;
+    public GameObject left_elbowTOleft_wrist;
+    public GameObject right_elbowTOright_wrist;
+
+    public GameObject backgroundTOleft_hip;
+    public GameObject backgroundTOright_hip;
+    public GameObject left_hipTOleft_knee;
+    public GameObject right_hipTOright_knee;
+    public GameObject left_kneeTOleft_ankle;
+    public GameObject right_kneeTOright_ankle;
+    public GameObject left_ankleTOleft_big_toe;
+    public GameObject left_ankleTOleft_heel;
+    public GameObject right_ankleTOright_big_toe;
+    public GameObject right_ankleTOright_heel;
+    public GameObject left_big_toeTOleft_small_toe;
+    public GameObject right_big_toeTOright_small_toe;
+
+
+    public void find_bodypart() 
+    {
+        nose = GameObject.Find( "nose" );
+        neck = GameObject.Find( "neck" );
+
+        right_shoulder = GameObject.Find( "right_shoulder" );
+        right_elbow = GameObject.Find( "right_elbow" );
+        right_wrist = GameObject.Find( "right_wrist" );
+
+        left_shoulder = GameObject.Find( "left_shoulder" );
+        left_elbow = GameObject.Find( "left_elbow" );
+        left_wrist = GameObject.Find( "left_wrist" );
+
+        right_hip = GameObject.Find( "right_hip" );
+        right_knee = GameObject.Find( "right_knee" );
+        right_ankle = GameObject.Find( "right_ankle" );
+
+        left_hip = GameObject.Find( "left_hip" );
+        left_knee = GameObject.Find( "left_knee" );
+        left_ankle = GameObject.Find( "left_ankle" );
+
+        right_eye = GameObject.Find( "right_eye" );
+        left_eye = GameObject.Find( "left_eye" );
+        right_ear = GameObject.Find( "right_ear" );
+        left_ear = GameObject.Find( "left_ear" );
+
+        left_big_toe = GameObject.Find( "left_big_toe" );
+        left_small_toe = GameObject.Find( "left_small_toe" );
+        left_heel = GameObject.Find( "left_heel" );
+
+        right_big_toe = GameObject.Find( "right_big_toe" );
+        right_small_toe = GameObject.Find( "right_small_toe" );
+        right_heel = GameObject.Find( "right_heel" );
+
+        background = GameObject.Find( "background" );
+    }
+
+    public void find_bodypart_edge()
+    {
+        noseTOleft_eye = GameObject.Find("noseTOleft_eye");
+        noseTOright_eye = GameObject.Find("noseTOright_eye");
+        left_eyeTOleft_ear = GameObject.Find("left_eyeTOleft_ear");
+        right_eyeTOright_ear = GameObject.Find("right_eyeTOright_ear");
+
+        noseTOneck = GameObject.Find("noseTOneck");
+        neckTObackground = GameObject.Find("neckTObackground");
+
+        neckTOleft_shoulder = GameObject.Find("neckTOleft_shoulder");
+        neckTOright_shoulder = GameObject.Find("neckTOright_shoulder");
+        left_shoulderTOleft_elbow = GameObject.Find("left_shoulderTOleft_elbow");
+        right_shoulderTOright_elbow = GameObject.Find("right_shoulderTOright_elbow");
+        left_elbowTOleft_wrist = GameObject.Find("left_elbowTOleft_wrist");
+        right_elbowTOright_wrist = GameObject.Find("right_elbowTOright_wrist");
+
+        backgroundTOleft_hip = GameObject.Find("backgroundTOleft_hip");
+        backgroundTOright_hip = GameObject.Find("backgroundTOright_hip");
+        left_hipTOleft_knee = GameObject.Find("left_hipTOleft_knee");
+        right_hipTOright_knee = GameObject.Find("right_hipTOright_knee");
+        left_kneeTOleft_ankle = GameObject.Find("left_kneeTOleft_ankle");
+        right_kneeTOright_ankle = GameObject.Find("right_kneeTOright_ankle");
+        left_ankleTOleft_big_toe = GameObject.Find("left_ankleTOleft_big_toe");
+        left_ankleTOleft_heel = GameObject.Find("left_ankleTOleft_heel");
+        right_ankleTOright_big_toe = GameObject.Find("right_ankleTOright_big_toe");
+        right_ankleTOright_heel = GameObject.Find("right_ankleTOright_heel");
+        left_big_toeTOleft_small_toe = GameObject.Find("left_big_toeTOleft_small_toe");
+        right_big_toeTOright_small_toe = GameObject.Find("right_big_toeTOright_small_toe");
+    }
+
+    private void PoseActiveControl(GameObject edge, GameObject part1, GameObject part2)
+    {
+        if (part1.transform.localScale == Vector3.zero || part2.transform.localScale == Vector3.zero)
+        {
+            edge.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+    }
+
+    public void EdgeActiveControls() 
+    {
+        find_bodypart();
+        find_bodypart_edge();
+
+        // face 
+        PoseActiveControl(noseTOleft_eye, nose, left_eye);
+        PoseActiveControl(noseTOright_eye, nose, right_eye);
+        PoseActiveControl(left_eyeTOleft_ear, left_eye, left_ear);
+        PoseActiveControl(right_eyeTOright_ear, right_eye, right_ear);
+
+        // body
+        PoseActiveControl(noseTOneck, nose, neck);
+        PoseActiveControl(neckTObackground, neck, background);
+
+        // arm
+        PoseActiveControl(neckTOleft_shoulder, neck, left_shoulder);
+        PoseActiveControl(neckTOright_shoulder, neck, right_shoulder);
+        PoseActiveControl(left_shoulderTOleft_elbow, left_shoulder, left_elbow);
+        PoseActiveControl(right_shoulderTOright_elbow, right_shoulder, right_elbow);
+        PoseActiveControl(left_elbowTOleft_wrist, left_elbow, left_wrist);
+        PoseActiveControl(right_elbowTOright_wrist, right_elbow, right_wrist);
+
+        // leg
+        PoseActiveControl(backgroundTOleft_hip, background, left_hip);
+        PoseActiveControl(backgroundTOright_hip, background, right_hip);
+        PoseActiveControl(left_hipTOleft_knee, left_hip, left_knee);
+        PoseActiveControl(right_hipTOright_knee, right_hip, right_knee);
+        PoseActiveControl(left_kneeTOleft_ankle, left_knee, left_ankle);
+        PoseActiveControl(right_kneeTOright_ankle, right_knee, right_ankle);
+        PoseActiveControl(left_ankleTOleft_big_toe, left_ankle, left_big_toe);
+        PoseActiveControl(left_ankleTOleft_heel, left_ankle, left_heel);
+        PoseActiveControl(right_ankleTOright_big_toe, right_ankle, right_big_toe);
+        PoseActiveControl(right_ankleTOright_heel, right_ankle, right_heel);
+        PoseActiveControl(left_big_toeTOleft_small_toe, left_big_toe, left_small_toe);
+        PoseActiveControl(right_big_toeTOright_small_toe, right_big_toe, right_small_toe);
+    }
 }
